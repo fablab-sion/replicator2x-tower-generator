3f349b058e46e26ace5ffb89b1017fa6b341d104
Alex Siero <alexsierro@gmail.com>
2016-03-11 21:21:28 +0100

    Now with parameters

--- autoTower.py
+++ autoTower.py
@@ -1,34 +1,73 @@
 #!/usr/bin/python
-
+import argparse
 import sys
 from subprocess import call
 
-# Define temperatures
-temperatures = sys.argv[1:len(sys.argv)]
-temperaturesString = '"' + ('","'.join(temperatures)) + '"'
+ARGS = None
 
-# Change scad file
-with open('tower.scad', 'r') as scadBaseFile:
-  with open('towerOut.scad', 'w') as scadOutFile:
-    scadOutFile.write(scadBaseFile.read().replace('###NumberOfBlocks###', str(len(temperatures))).replace('###Labels###', temperaturesString))
-# Generate stl
-call(["openscad", "-o", "tower.stl", "towerOut.scad"])
-# Slice, output in .gcode
-call(["miracle_grue", "-c", "miracle.json", "-o", "tower.gcode", "tower.stl"])
+def main(temperatures, bedTemp):
+	temperaturesString = ",".join(['"{}"'.format(x) for x in temperatures])
+	print temperaturesString
+	# Change scad file
+	with open('tower.scad', 'r') as scadBaseFile:
+	  with open('towerOut.scad', 'w') as scadOutFile:
+	    scadOutFile.write(scadBaseFile.read().replace('###NumberOfBlocks###', str(len(temperatures))).replace('###Labels###', temperaturesString))
+	# Generate stl
+	call(["openscad", "-o", "tower.stl", "towerOut.scad"])
+	# Slice, output in .gcode
+	call(["miracle_grue", "-c", "miracle.json", "-o", "tower.gcode", "tower.stl"])
 
-# Replace temperatures in gcode
-with open('tower.gcode', 'r') as gcodeFile:
-  with open('header.gcode', 'r') as gcodeHeaderFile:
-    with open('towerOut.gcode', 'w') as gcodeOutFile:
-      #set first temperature in header
-      gcodeHeader = gcodeHeaderFile.read().replace('###SetTemp###', 'M104 S' + str(temperatures[0]) + ' T0')
-      gcodeString =  gcodeFile.read()
+	# Replace temperatures in gcode
+	with open('tower.gcode', 'r') as gcodeFile:
+	  with open('header.gcode', 'r') as gcodeHeaderFile:
+	    with open('towerOut.gcode', 'w') as gcodeOutFile:
+	      #set first temperature in header
+	      gcodeHeader = gcodeHeaderFile.read().replace('###ExtruderTemp###', str(temperatures[0])).replace('###BedTemp###', str(bedTemp))
+	      gcodeString =  gcodeFile.read()
 
-      for tempNumber in range(1, len(temperatures)):
-        index =  gcodeString.find('Z' + str(tempNumber*10))
-        gcodeString = gcodeString[:index-18]+'\nM104 S' + str(temperatures[tempNumber])+' T0 \n'+gcodeString[index-17:len(gcodeString)]
+	      for tempNumber in range(1, len(temperatures)-1):
+	        index =  gcodeString.find('Z' + str(tempNumber*10))
+	        gcodeString = gcodeString[:index-18]+'\nM104 S' + str(temperatures[tempNumber])+' T0 \n'+gcodeString[index-17:len(gcodeString)]
 
-      gcodeOutFile.write(gcodeHeader + gcodeString)
+	      gcodeOutFile.write(gcodeHeader + gcodeString)
 
-# Convert gcode to x3g
-call(["gpx", "-v", "towerOut.gcode", "tower.x3g"])
+	# Convert gcode to x3g
+	call(["gpx", "-v", "towerOut.gcode", "tower.x3g"])
+
+
+def parse_arguements():
+    """Parse the command line arguments."""
+    # Define a parser
+    parser = argparse.ArgumentParser(description="Generate temperature calibration tower for Makerbot Replicator2x (g3x)")
+    parser.add_argument('--list', '-l', type=int, nargs='*',
+    					default=None,
+                        help='list of temperatures')
+    parser.add_argument('--bed', '-b', type=int,
+                        default='110',
+                        help='temperature of the bed')
+    parser.add_argument('--from', '-f', type=int, dest='start',
+    					default=None,
+                        help='from temperature')
+    parser.add_argument('--to', '-t', type=int,
+    					default=None,
+                        help='to temperature')
+    parser.add_argument('--step', '-s', type=int,
+    					default=None,
+                        help='temperature step')
+    # Parse the arguments
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    # Parse arguments
+    ARGS = parse_arguements()
+    if ARGS.list is not None:
+    	temperatures = ARGS.list
+    elif ARGS.start is not None and ARGS.to is not None and ARGS.step is not None:
+    	temperatures = list(range(ARGS.start, ARGS.to + 1, ARGS.step))
+    else:
+    	sys.exit(1)
+    print(temperatures)
+    print(ARGS.bed)
+
+    # Run the application
+    sys.exit(main(temperatures, ARGS.bed))
--- header.gcode
+++ header.gcode
@@ -9,9 +9,9 @@ M132 X Y Z A B (Recall stored home offsets for XYZAB axis)
 G92 X152 Y72 Z0 A0 B0
 G1 X-112 Y-73 Z150 F3300.0 (move to waiting position)
 G130 X20 Y20 A20 B20 (Lower stepper Vrefs while heating)
-M109 S80 T0
+M109 S###BedTemp### T0
 M134 T0
 M135 T0
-###SetTemp###
+M104 S###ExtruderTemp### T0
 M133 T0
 G130 X127 Y127 A127 B127 (Set Stepper motor Vref to defaults)
